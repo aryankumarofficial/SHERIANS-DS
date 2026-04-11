0168c5bfa55bc76ffd128d4152c0d3e3ac387518
Aryan Kumar <aryanak9163@gmail.com>
2026-04-11 18:41:26 +0530

    class 2: completes the varible and comments in python

---
+++ module_01_python_powerhouse/class_02/data-types.py
@@ -0,0 +1,40 @@
+"""
+Data Types in Python:
+    Majorly 3:
+        - numbers
+            - int
+            - float
+            - complext
+        - string
+        - boolean
+"""
+
+# numbers
+
+a = 45  # int
+print(type(a))  # <class 'int'>
+
+b = 12 / 3  # float
+print(type(b))  # <class 'float'>
+
+c = 12j  # complex
+print(type(c))  # <class 'complex'>
+
+
+# String
+
+# str = "hello, Myself is Aryan"
+str = 'Hello, myself is "Aryan"'  # str
+
+print(type(str))  # <class 'str'>
+
+# boolean
+
+isReading = False  # bool
+
+print(type(isReading))  # <class 'bool'>
+
+x = 5
+while x:
+    print(x)
+    x -= 1
---
+++ module_01_python_powerhouse/class_02/main.py
@@ -0,0 +1,21 @@
+print("Welcome to the comments and variables bootcamp!")
+
+"""
+Hello JI
+"""
+
+# Hello Sir Welcome to DS world
+
+# Variables section
+
+a = 10
+
+# Naming convention
+
+# casing:
+# camel case
+aryanKumar = 10
+# pascal case
+AryanKumar = 32
+# snake case
+aryan_kumar = 321
