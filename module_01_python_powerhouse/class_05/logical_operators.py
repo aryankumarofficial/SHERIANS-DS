c9e98eda9bd40ae5acd22429460ab591bf7d87ed
Aryan Kumar <aryanak9163@gmail.com>
2026-04-21 15:02:03 +0530

    class 05: completes the comparision and logical operators

---
+++ module_01_python_powerhouse/class_05/logical_operators.py
@@ -0,0 +1,9 @@
+print(12 == 12 and 56 > 100 and 10 == 10)
+
+print(12 == 56 or 45 == 45 or 34 == 98)
+
+print(not 12 == 12)  # reverse the bool val
+
+print(not 12 == 12 == False)
+
+print(bool(0))
---
+++ module_01_python_powerhouse/class_05/main.py
@@ -0,0 +1,12 @@
+# comparision operator
+# ==, !=,<=,>=,<,>
+a = 12
+b = 45
+print(b > a)  # grater than
+
+print(12 == 12)  # equals to
+print(12 != 12)  # not equals to
+print(12 < 13)  # smaller than
+print(12 <= 12)  # smaller than or equal to
+
+print((12 == 12) == True)  # True
