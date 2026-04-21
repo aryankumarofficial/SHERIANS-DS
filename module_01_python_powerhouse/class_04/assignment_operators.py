72cdd42b1fbd2c62cadf9f32cdb52a6eeab083dd
Aryan Kumar <aryanak9163@gmail.com>
2026-04-21 10:58:44 +0530

    class 4: completes the orperators

---
+++ module_01_python_powerhouse/class_04/assignment_operators.py
@@ -0,0 +1,12 @@
+# Assignment Operators
+
+a = 12
+b = 34
+
+a += 12  # a + 12
+print(a)
+
+str1 = "well "
+str2 = "hello"
+
+print(str1 * 2 + str2)
---
+++ module_01_python_powerhouse/class_04/main.py
@@ -0,0 +1,29 @@
+# Arithmetic operators
+# 7 of them: +, -, *, /, //, **, %
+
+# py follows BODMAS rule -  Brack of Division Mul Add Sub
+
+a = 10
+b = 12
+print(a + b + 19 + 23 + 89)  # adds variables
+print(a - b)  # substracts variables
+
+print(12 + 12 - 24)  # BODMAS
+
+# multiplication
+print(12 * 2)
+
+# Division
+print(12 / 2)
+
+# floor division
+print(12 // 5)  # converts to int after division
+
+# exponetial
+print(5**2)  # 5^2
+
+# mod -> remainders
+
+print(36 % 5)
+
+print((12 + 45) / 56)
