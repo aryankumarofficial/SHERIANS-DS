17bf615265e0331e85bee88f9b405ceb4a9bdac1
aryan <aryanak9163@gmail.com>
2026-09-15 01:25:48 +0530

    while loop class

---
+++ module_01_python_powerhouse/classroom/class_08/main.py
@@ -0,0 +1,17 @@
+# a = 1
+#
+# while a <= 10:
+#     print(a)
+#     a += 1
+
+# print from 10 to 0
+n = 10
+while n != 0:
+    if n == 7:
+        n -= 1
+        continue
+    else:
+        if n == 3:
+            break
+    print(n)
+    n -= 1
