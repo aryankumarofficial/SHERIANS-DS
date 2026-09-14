d707929a4955c8d062c95129db94a5fef2e32c02
aryan <aryanak9163@gmail.com>
2026-09-14 23:46:19 +0530

    class 07

---
+++ module_01_python_powerhouse/classroom/class_07/main.py
@@ -0,0 +1,38 @@
+r = range(1, 11, 1)
+
+# for i in r:
+#     # print(i)
+#     print("Hello")
+
+
+# print from 10 to 40
+# for i in range(10, 41):
+#     print(i)
+
+# print from -10 to 20
+# for i in range(-10,21):
+#     print(i)
+
+
+# print from 34 to 5
+# for i in range(34,4 ,-1):
+#     print(i)
+
+# print table of 5
+# for i in range(5, 51, 5):
+#     print(i)
+
+# print table by user input
+# n = int(input("Enter the number to print the Table:-"))
+#
+# for i in range(n, n * 10 + 1, n):
+#     print(i)
+
+# loop on strings
+s = "Hello how are your!"
+
+# for i in s:  # first approach
+#     print(i)
+
+for i in range(len(s)):
+    print(s[i])
