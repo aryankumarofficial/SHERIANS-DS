0b1dc396e4d7ef897ed56162e6636b2b33cbd66d
aryan <aryanak9163@gmail.com>
2026-09-19 03:28:51 +0530

    dict class

---
+++ module_01_python_powerhouse/classroom/class_14/main.py
@@ -0,0 +1,40 @@
+# Dictionary
+
+# key value pairs
+# d = {'a': 10, 'b': 20, 'c': 30}
+# print(d)
+# d["a"] = 100
+
+# print(d)
+
+# constructor
+
+# d = dict(name="Aryan", age=24, gender="Male")
+# print(d)
+
+# d = dict([("name", "Aryan"), ("Age", 24)])
+# print(d)
+
+# traversing
+# a = {10: 100, 20: 200, 30: 300, 40: 400, 50: 500}
+
+# for i in a:
+#     print(a[i])
+
+# for i in a.values():
+#     print(i)
+
+# methods
+# a = {10: 100, 20: 200, 30: 300, 40: 400, 50: 500}
+
+# print(a.get(90)) # Default to None
+# print(a.keys())
+# print(a.pop(30)) # 300
+# print(a.popitem())  # (50,500)
+# a.setdefault(90,1222)  # with default value to None
+# print(a)
+
+# a = {10: 100, 20: 200, 30: 300, 40: 400, 50: 500}
+# b = {60: 600, 70: 700, 80: 800}
+# a.update(b)
+# print(a)
