8154c73152cb4f94c7272b7fcf164be4a5e580f2
aryan <aryanak9163@gmail.com>
2026-09-19 01:02:12 +0530

    tuple class

---
+++ module_01_python_powerhouse/classroom/class_12/main.py
@@ -0,0 +1,39 @@
+# tuples
+# heterogeneous
+# immutable
+# duplicate allowed
+
+# t = (1, 2, 3, 4, 5, 6)
+# print(t)
+
+# tuple unpacking
+# a, b, c = (10, 20, 30)
+# print(a)
+# a = (10) # unpacked
+# print(type(a))
+
+# indexing and slicing
+# a = (1, 2, 3, 4, 5, 6)
+# print(a[:2])
+
+# a = [10, 20, 30, 40]
+
+# tup = tuple(a)  # type conversion
+
+# print(type(a))  # list
+# print(type(tup))  # tuple
+
+# traversing
+
+# t = (10, 20, 30, 40, 50)
+
+# for i in t:
+#     print(i)
+
+# for i in range(len(t)):
+#     print(t[i])
+
+# tuple methods
+a = [10, 20, 30, 40, 40, 40, 50, 30, 50]
+# print(a.count(30)) # freq
+# print(a.index(30)) # idx
